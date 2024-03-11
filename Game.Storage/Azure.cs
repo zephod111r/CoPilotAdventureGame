23a0aee1d38f5dbb949f85b8411f1c15b2089016
zephod111r <github@zephod.co.uk>
2024-03-11 14:50:11 +0000

    Add files via upload

---
+++ Game.Storage/Azure.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+using Azure.Storage.Files.Shares;
+using Azure.Data.Tables;
+
+namespace Game.Storage.Azure
+{
+    class Storage
+    {
+        public BlobServiceClient? blobServiceClient { get; }
+        public QueueServiceClient? queueServiceClient { get; }
+        public ShareServiceClient? shareServiceClient { get; }
+        public TableServiceClient? tableServiceClient { get; }
+
+        public Storage Singleton { get; } = new Storage();
+
+        private Storage()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+
+            string? connectionString = config["AzureWebJobsStorage"];
+
+            if(connectionString == null)
+            {
+                throw new System.Exception("AzureWebJobsStorage is not set");
+            }
+
+            blobServiceClient = new BlobServiceClient(connectionString);
+            queueServiceClient = new QueueServiceClient(connectionString);
+            shareServiceClient = new ShareServiceClient(connectionString);
+            tableServiceClient = new TableServiceClient(connectionString);
+        }
+    }
+}
