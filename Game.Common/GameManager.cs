5ab13214495a81a7c19dd392b7416287cf670542
Simon Abykov <simon.abykov@metaswitch.com>
2024-03-11 12:34:01 +0000

    Initial project

---
+++ Game.Common/GameManager.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Game.Common.Manager
+{
+    public class GameManager
+    {
+        public void Start()
+        {
+            while (true)
+            {
+                // Game loop
+            }
+        }
+    }
+}
---
+++ Game.Launcher.Console/Launcher.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Game.Common.Manager;
+
+Console.WriteLine("Starting...");
+
+GameManager gameManager = new GameManager();
+gameManager.Start();
