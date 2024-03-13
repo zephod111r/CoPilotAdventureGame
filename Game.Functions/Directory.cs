7fd4c1adc927df62ce6d0c381839a7d6b18a1dc0
zephod111r <github@zephod.co.uk>
2024-03-13 20:43:16 +0000

    Finding out the directory structure

---
+++ Game.Functions/Directory.cs
@@ -0,0 +1,39 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Game.Functions
+{
+    public static class Directory
+    {
+        [Function(nameof(Directory))]
+        public static async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = nameof(Directory))] HttpRequestData req,
+            FunctionContext context)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            Dictionary<string, string> fileMap = new Dictionary<string, string>();
+
+            DirectoryInfo info = new DirectoryInfo(".");
+            foreach (var item in info.EnumerateDirectories())
+            {
+                foreach (var item1 in item.EnumerateFiles())
+                {
+                    fileMap.Add(item1.FullName, item.FullName);
+                }
+            };
+
+            string jsonMap = JsonConvert.SerializeObject(fileMap);
+            byte[] bytearray = Encoding.UTF8.GetBytes(jsonMap);
+            response.Body = new MemoryStream(bytearray);
+            response.Headers.Add("content-type", "text/html");
+            return response;
+        }
+    }
+}
--- Game.Functions/Root.cs
+++ Game.Functions/Root.cs
@@ -1,23 +1,24 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using System;
+using System.Diagnostics.Eventing.Reader;
 using System.IO;
-using System.Net.Http;
 using System.Net;
 using System.Threading.Tasks;
-using System.Net.Http.Headers;
 
 namespace Game.Functions
 {
     public static class Root
     {
-        
+
 
         [Function(nameof(Root))]
         public static async Task<HttpResponseData> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "/")] HttpRequestData req,
-            FunctionContext executionContext)
+            ExecutionContext context)
         {
             var response = req.CreateResponse(HttpStatusCode.OK);
+
             FileStream stream = new FileStream(@"wwwroot\index.html", FileMode.Open, FileAccess.Read);
             response.Body = stream;
             response.Headers.Add("content-type", "text/html");
