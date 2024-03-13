a71e3dbf3a877db711c6c3749e49b242f83ce3f9
zephod111r <github@zephod.co.uk>
2024-03-13 21:15:51 +0000

    Pathing fixes

--- Game.Functions/Root.cs
+++ Game.Functions/Root.cs
@@ -4,13 +4,26 @@ using System;
 using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Net;
+using System.Security.Policy;
 using System.Threading.Tasks;
 
 namespace Game.Functions
 {
     public static class Root
     {
+        static string nameOfRoot { get; set; }
 
+        static string root() {
+            DirectoryInfo info = new DirectoryInfo(".");
+            foreach (var item in info.EnumerateDirectories())
+            {
+                if(item.Name == "wwwroot")
+                {
+                    return item.FullName;
+                }
+            };
+            return ".";
+        }
 
         [Function(nameof(Root))]
         public static async Task<HttpResponseData> Run(
@@ -19,7 +32,14 @@ namespace Game.Functions
         {
             var response = req.CreateResponse(HttpStatusCode.OK);
 
-            FileStream stream = new FileStream(@"wwwroot\index.html", FileMode.Open, FileAccess.Read);
+            if(nameOfRoot == null)
+            {
+                nameOfRoot = root();
+            }
+
+            string path = string.Concat(@nameOfRoot, @"\index.html");
+
+            FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
             response.Body = stream;
             response.Headers.Add("content-type", "text/html");
             return response;
